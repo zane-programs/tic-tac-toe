81ba0a8f18fecaf818b3c832675d342412e5bd8c
zane-programs <zstjohn22@windwardschool.org>
2020-11-27 20:20:48 -0800

    code splitting

---
+++ src/components/Board/Square.jsx
@@ -0,0 +1,53 @@
+import { useCallback, useContext, useMemo, useState } from "react";
+import GameContext from "../../context/GameContext";
+
+const PLAYER_COLORS = {
+  X: "#f00",
+  O: "#00f",
+};
+
+export default function Square(props) {
+  const { squares, setSquares, player, setPlayer, winner } = useContext(
+    GameContext
+  );
+
+  const [hoverState, setHoverState] = useState(false);
+
+  const currentSquareState = useMemo(() => squares[props.index], [
+    squares,
+    props.index,
+  ]);
+
+  const handleSquareClick = useCallback(
+    (squareIndex) => {
+      if (!winner && !squares[squareIndex]) {
+        let squaresCopy = squares.slice();
+        squaresCopy[squareIndex] = player; // X or O
+        setPlayer(player === "X" ? "O" : "X"); // other player plays now
+        setSquares(squaresCopy);
+      }
+    },
+    [squares, setSquares, player, setPlayer, winner]
+  );
+
+  const squareColor = useMemo(() => {
+    if (currentSquareState === null) return "#aaa";
+    return PLAYER_COLORS[currentSquareState];
+  }, [currentSquareState]);
+
+  return (
+    <td
+      onClick={() => handleSquareClick(props.index)}
+      onMouseOver={() => setHoverState(true)}
+      onMouseOut={() => setHoverState(false)}
+    >
+      <span style={{ color: squareColor }}>
+        {currentSquareState
+          ? currentSquareState
+          : hoverState && !winner
+          ? player
+          : null}
+      </span>
+    </td>
+  );
+}
--- src/components/Board/index.jsx
+++ src/components/Board/index.jsx
@@ -1,11 +1,5 @@
 import styles from "./Board.module.css";
-import GameContext from "../../context/GameContext";
-import { useCallback, useContext, useMemo, useState } from "react";
-
-const PLAYER_COLORS = {
-  X: "#f00",
-  O: "#00f",
-};
+import Square from "./Square";
 
 export default function Board() {
   return (
@@ -26,51 +20,5 @@ function Row(props) {
         <Square key={index} index={index} />
       ))}
     </tr>
-  );
-}
-
-function Square(props) {
-  const { squares, setSquares, player, setPlayer, winner } = useContext(
-    GameContext
-  );
-
-  const [hoverState, setHoverState] = useState(false);
-
-  const currentSquareState = useMemo(() => squares[props.index], [
-    squares,
-    props.index,
-  ]);
-
-  const handleSquareClick = useCallback(
-    (squareIndex) => {
-      if (!winner && !squares[squareIndex]) {
-        let squaresCopy = squares.slice();
-        squaresCopy[squareIndex] = player; // X or O
-        setPlayer(player === "X" ? "O" : "X"); // other player plays now
-        setSquares(squaresCopy);
-      }
-    },
-    [squares, setSquares, player, setPlayer, winner]
-  );
-
-  const squareColor = useMemo(() => {
-    if (currentSquareState === null) return "#aaa";
-    return PLAYER_COLORS[currentSquareState];
-  }, [currentSquareState]);
-
-  return (
-    <td
-      onClick={() => handleSquareClick(props.index)}
-      onMouseOver={() => setHoverState(true)}
-      onMouseOut={() => setHoverState(false)}
-    >
-      <span style={{ color: squareColor }}>
-        {currentSquareState
-          ? currentSquareState
-          : hoverState && !winner
-          ? player
-          : null}
-      </span>
-    </td>
   );
 }
